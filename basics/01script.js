27c31b72f939ab832d81f713092e8e3187e169dc
DeepsP07 <107102038+DeepsP07@users.noreply.github.com>
2023-12-19 19:21:49 +0000

    conversionfile

--- basics/01script.js
+++ basics/01script.js
@@ -1,1 +1,10 @@
 console.log("Deepali")
+var name="Deepali"
+const accountId =3212
+var data;
+var rest =null
+/*to print the all values at a time we can use table */
+
+console.table([name ,accountId, data,rest])
+
+console.table([typeof name,typeof accountId,typeof data,typeof rest])
---
+++ basics/02converstion.js
@@ -0,0 +1,20 @@
+// let isNumber ="33asc"
+// let valueIsValue = Number(isNumber)
+// console.log(valueIsValue)
+// console.log(typeof valueIsValue)
+/* 33 => number
+"33"=>33 number
+null=>0 number
+"33asa" => NaN Not A Number
+*/
+
+let oneNum ="ase123"
+let conBoolean = Boolean(oneNum)
+console.log(conBoolean)
+/*"12qqw" => true
+123=>true
+0=>false */
+/*----*****operations*****-----*/
+let type =9
+++type
+console.log(type)
